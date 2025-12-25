0c5422fafac5cb5004e30a58b36273c4aa537993
Kz1232 <98634693+Kz1232@users.noreply.github.com>
2025-12-25 21:36:48 +0545

    chore(speakers):added speakers image and metadata

---
+++ src/data/speakers.ts
@@ -0,0 +1,112 @@
+import { IspeakerCard } from "@/interface/ISpeakers";
+
+export const SpeakerCardData: IspeakerCard[] = [
+  {
+    id: "1",
+    coverImage: "/speakers/shubham 1.png",
+    name: "SHUBHAM POKHREL",
+    occupation: "Honorary Chair, AWS Cloud Club at TU | DevOps Engineer",
+  },
+  {
+    id: "2",
+    coverImage: "/speakers/Suraj 1.png",
+    name: "SURAJ BHATTRAI",
+    occupation: "AWS Certified Solutions Architect Associate",
+  },
+  {
+    id: "3",
+    coverImage: "/speakers/BinishaJoshi.png",
+    name: "Binisha Joshi",
+    occupation: " AWS Community Builder & Software Engineer",
+  },
+  {
+    id: "4",
+    coverImage: "/speakers/LajahShrestha.png",
+    name: "Lajah Shrestha",
+    occupation: "AWS Community Builder & Dev/Clops Engineer",
+  },
+  {
+    id: "5",
+    coverImage: "/speakers/Akrur 1.png",
+    name: "AKRUR NEUPANE",
+    occupation: "AWS Community Builder | Cloud Ops Engineer",
+  },
+  {
+    id: "6",
+    coverImage: "/speakers/APil 1.png",
+    name: "APIL CHAUDHARY",
+    occupation: "AWS Certified Solutions Architect",
+  },
+  {
+    id: "7",
+    coverImage: "/speakers/Nirjal 1.png",
+    name: "NIRJAL BHURTEL",
+    occupation: "Embedded Software Architect",
+  },
+  {
+    id: "8",
+    coverImage: "/speakers/Raju 1.png",
+    name: "Raju Mandal",
+    occupation: "Senior Data and AI Engineer",
+  },
+  {
+    id: "9",
+    coverImage: "/speakers/Binod 1.png",
+    name: "Binod Adhikari",
+    occupation: "Senior Software Engineer",
+  },
+  {
+    id: "10",
+    coverImage: "/speakers/Rishikesh 1.png",
+    name: "RISHIKESH GAUTAM",
+    occupation: "AWS Community Builder",
+  },
+  {
+    id: "11",
+    coverImage: "/speakers/Shushovan 1 (1).png",
+    name: "SHUSHOVAN PAUDYAL",
+    occupation: "Cloud Consultant",
+  },
+  {
+    id: "12",
+    coverImage: "/speakers/Aayush Pokhrel.png",
+    name: "Aayush Pokharel",
+    occupation: "Devops Engineer",
+  },
+  {
+    id: "13",
+    coverImage: "/speakers/Raman 1.png",
+    name: "RAMAN POKHAREL",
+    occupation: "Game Developer",
+  },
+  {
+    id: "14",
+    coverImage: "/speakers/Abiral Sangaroula.png",
+    name: "ABIRAL SANGROULA",
+    occupation: "Principal Engineer, Qsystems AI",
+  },
+  {
+    id: "15",
+    coverImage: "/speakers/Nischal.png",
+    name: "NISCHAL SHAKYA",
+    occupation: "Software Engineer, Qsystems AI",
+  },
+  {
+    id: "16",
+    coverImage: "/speakers/Devi Bhattrai.png",
+    name: "DEVI BHATTRAI",
+    occupation: "Leader, AWS UG Women in Tech",
+  },
+  {
+    id: "17",
+    coverImage: "/speakers/Upendra 1.png",
+    name: "UPENDRA PRASAD NEUPANE",
+    occupation: "Founder, AWS Cloud Club at TU",
+  },
+  {
+    id: "18",
+    coverImage: "/speakers/Thakur Paudel.png",
+    name: "THAKUR PAUDEL",
+    occupation: "Embedded and Electrical Lead at Yatri Motors",
+  },
+];
